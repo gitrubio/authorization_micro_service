9914bb2bb9273896fb83a6798f06497a2653b798
gitrubio <carloviloria0@gmail.com>
2024-01-20 15:11:06 -0500

    fix : cors

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,13 @@ import productsRoutes from './routes/products.routes';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
 import { createRoles } from './libs/initialSetup';
-
+import cors from 'cors';
 const app = express();
 createRoles()
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 app.use("/api/products",productsRoutes);
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
