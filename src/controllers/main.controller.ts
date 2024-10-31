c8731533de3d4aae49da0f11907939f67dd168ac
gitrubio <carloviloria0@gmail.com>
2024-10-31 01:44:49 -0500

    feat: add start script and enhance video generation prompts

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -5,7 +5,13 @@ import config from '../config';
 import { io } from '../index';
 
 const client = new RunwayML({ apiKey: config.RUNWAYML_API_SECRET });
-
+const prompts = [
+    "Transform the person's face into a cyborg, showing how advanced technology integrates into their being.",
+    "The image turns into a video where the person explores a digital landscape filled with data and holograms.",
+    "The person interacts with a virtual assistant, showcasing a fluid dialogue with futuristic graphics appearing around them.",
+    "The person's face blends into a futuristic city, where their expression changes as they interact with the environment.",
+    "The image of the person transforms into various futuristic versions of themselves, illustrating how they might look in different technological scenarios."
+];
 
 export const generateVideo = async (req: Request, res: Response) => {
 	try {
@@ -14,13 +20,13 @@ export const generateVideo = async (req: Request, res: Response) => {
 		if (!image) return res.status(STATUS_CODES.BAD_REQUEST).json({ error: 'No se ha proporcionado una imagen' });
 
 		console.log('Generating video...');
-
+		const indexpromp = Math.floor(Math.random() * prompts.length);
 		// Create a new image-to-video task using the "gen3a_turbo" model
 		const imageToVideo = await client.imageToVideo.create({
 			model: 'gen3a_turbo',
 			// Point this at your own image file
 			promptImage: image,
-			promptText: 'a video of a futuristic city with flying cars',
+			promptText: prompts[indexpromp],
 		});
 
 		const taskId = imageToVideo.id;
